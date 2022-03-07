8e8c2b0b6ac32d3db8e1f064e4b84c85ac6e9f85
Anas RAR <mynameanasrar@gmail.com>
2022-03-07 18:44:36 +1300

    clean up unused command

--- build.bat
+++ build.bat
@@ -2,14 +2,6 @@ if not exist build\ (
   mkdir build
 )
 
-if not exist sqlite3\sqlite3.lib (
-	cd sqlite3
-	lib /DEF:sqlite3.def /OUT:sqlite3.lib /MACHINE:x64
-	cd ..
-)
-
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake -G "Visual Studio 16 2019" -A x64 -S ..
 msbuild gah.sln /p:Configuration=Release /p:Platform=x64
-cd ..
-copy sqlite3\sqlite3.dll build\bin\Release
